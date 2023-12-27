8b833641dd895e252525da33f3ef1fe080212475
ChoiDazzi <choidazzi@gmail.com>
2023-12-27 19:35:57 +0900

    commit

---
+++ pages/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>hello. {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '신짱구'
+    }
+  },
+}
+</script>
---
+++ pages/study/duplRoute/Container/Container1.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>Container1</p>
+  </div>
+</template>
